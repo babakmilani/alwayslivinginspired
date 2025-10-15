521d67b476df24e12e3c6c92a69116e5145a3459
Babak Milani <babakmilani@Babaks-Laptop.local>
2025-10-15 12:24:14 +0200

    Deploy: Configure Vite to build to /docs for GitHub Pages

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,8 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  // 💡 Change 'dist' to 'docs'
+  globalIgnores(['docs']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // 💡 Configuration for GitHub Pages Deployment
+  base: "/alwayslivinginspired/",
+
+  build: {
+    outDir: 'docs', // Output to 'docs' folder for GitHub Pages
+  }
 })
