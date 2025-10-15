ff93299a9fa072abf66bb5a30d0c68e39b590b74
Babak Milani <babakmilani@Babaks-Laptop.local>
2025-10-15 15:50:17 +0200

    changed the vite config file'

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // If you are using React
 
 export default defineConfig({
+  // This tells Vite that all public assets (like your /blogs folder) 
+  // and links should be prefixed with '/docs/'.
+  base: '/docs/',
   plugins: [react()],
-
-  // For custom domains, use "/" as the base
-  base: '/',
-
-  build: {
-    outDir: 'docs',
-  }
-})
+});
