c49f6eb527855f57bb8090bce7353e57ff38a70e
Babak Milani <babakmilani@Babaks-Laptop.local>
2025-10-15 12:54:57 +0200

    fix: update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
 
-  // 💡 CRITICAL: Sets the base path for assets
-  base: "/",
+  // Change this to match your GitHub repository name
+  base: '/alwayslivinginspired/',
 
-  // 💡 Ensures the output goes to the 'docs' folder for GitHub Pages
   build: {
     outDir: 'docs',
   }
